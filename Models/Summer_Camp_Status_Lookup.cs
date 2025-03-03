3a4d74bf7ffc087d3e8832645cadd6946273dcab
Ronald Whitington <whitington@gmail.com>
2025-03-03 11:48:57 -0500

    Enable/Disable buttons.

---
+++ Models/Summer_Camp_Status_Lookup.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Summer_Camp_Admin.Models
+{
+  public class Summer_Camp_Status_Lookup
+  {
+    [Key]
+    public string? ID_Code { get; set; }
+
+    public string? Desc { get; set; }
+  }
+}
