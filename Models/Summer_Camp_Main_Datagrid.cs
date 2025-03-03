a2d741810617332617befbbf0540548b38bc42b3
Ronald Whitington <whitington@gmail.com>
2025-03-03 15:49:44 -0500

    Update columns and methods in Home.razor and datagrid class
    Updated Home.razor with new columns: FirstName, LastName, EthnicCode,
    Latino, Indian, Asian, Black, Pacific, and White. Rearranged existing
    columns. Simplified btnMsgbox method to log button clicks. Added log
    entry in btnSaveSelected method for jsonPayload data before HTTP POST.
    
    Updated Summer_Camp_Main_Datagrid.cs with new properties: FirstName,
    LastName, EthnicCode, Latino, Indian, Asian, Black, Pacific, and White.
    Rearranged existing properties.

--- Models/Summer_Camp_Main_Datagrid.cs
+++ Models/Summer_Camp_Main_Datagrid.cs
@@ -4,37 +4,55 @@ namespace Summer_Camp_Admin.Models
 {
   public class Summer_Camp_Main_Datagrid
   {
-      [Key]
-      public int ID { get; set; }
+    [Key]
+    public int ID { get; set; }
 
-      [MaxLength(10)]
-      public string? FK_Student_Registration__Student_ID { get; set; }
+    [MaxLength(10)]
+    public string? FK_Student_Registration__Student_ID { get; set; }
 
-      [MaxLength(500)]
-      public string? Summer_Camp_Title { get; set; }
+    public string? FirstName { get; set; }
 
-      [MaxLength(20)]
-      public string? WeekNbr { get; set; }
+    public string? LastName { get; set; }
 
-      [MaxLength(200)]
-      public string? SchoolName { get; set; }
+    public string? EthnicCode { get; set; }
 
-      [MaxLength(4)]
-      public string? School_ID { get; set; }
+    public string? Latino { get; set; }
 
-      [MaxLength(200)]
-      public string? Location { get; set; }
+    public string? Indian { get; set; }
 
-      [MaxLength(200)]
-      public string? Level { get; set; }
+    public string? Asian { get; set; }
 
-      [MaxLength(200)]
-      public string? Grade { get; set; }
+    public string? Black { get; set; }
 
-      [MaxLength(1)]
-      public string? FK_Status { get; set; }
+    public string? Pacific { get; set; }
 
-      [MaxLength(10)]
-      public string? CalendarYR { get; set; }
+    public string? White { get; set; }
+
+    [MaxLength(500)]
+    public string? Summer_Camp_Title { get; set; }
+
+    [MaxLength(20)]
+    public string? WeekNbr { get; set; }
+
+    [MaxLength(200)]
+    public string? SchoolName { get; set; }
+
+    [MaxLength(4)]
+    public string? School_ID { get; set; }
+
+    [MaxLength(200)]
+    public string? Location { get; set; }
+
+    [MaxLength(200)]
+    public string? Level { get; set; }
+
+    [MaxLength(200)]
+    public string? Grade { get; set; }
+
+    [MaxLength(1)]
+    public string? FK_Status { get; set; }
+
+    [MaxLength(10)]
+    public string? CalendarYR { get; set; }
   }
 }
